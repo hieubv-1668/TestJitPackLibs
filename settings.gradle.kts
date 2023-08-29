3fa629f92c9b47f221250156ef8bf6aaa04326a7
hieubv-1668 <bui.van.hieu@sun-asterisk.com>
2023-08-29 10:40:21 +0700

    [Add] Config publish modules lib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Add this maven
         gradlePluginPortal()
     }
 }
@@ -10,9 +11,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Add this maven
     }
 }
 
 rootProject.name = "TestJitPackLibs"
 include(":app")
- 
+include(":core")
